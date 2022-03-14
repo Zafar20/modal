3c8d399a96d9902dd54a7a7f60cc9d5bb647d3c9
Zafar <ertaevzafar@gmail.com>
2022-03-14 09:12:41 +0300

    modal

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="wrapper">
+      <div class="wrapper-content">
+        <section>
+          <div class="container">
+            <button @click="modalFirst = !modalFirst" class="btn btnPrimary">Show First Modal</button>
+            <Modal 
+              title="First Modal"
+              v-if="modalFirst"
+              @close="modalFirst = !modalFirst">
+              <div slot="body">
+                <p>Text by Zafar</p>
+                <button class="btn btnPrimary">Well done</button>
+              </div>
+            </Modal>
+          </div>
+        </section>
+      </div>
+    </div>
+</template>
+
+<script>
+
+import Modal from '@/components/Modal.vue';
+
+export default {
+  components:{
+    Modal
+  },
+  data() {
+    return {
+      modalFirst: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
